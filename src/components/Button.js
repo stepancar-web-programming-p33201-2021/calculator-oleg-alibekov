b8b6380ee1ef4ad5e5d4258882b77a1cd1dc47f2
olegalibekov <alioleg8@gmail.com>
2021-11-06 17:58:26 +0300

    Add tab navigation

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import "./Button.css";
 
 const Button = ({ className, value, onClick }) => {
     return (
-        <button className={className} onClick={onClick}>
+        <button className={className} onClick={onClick} >
             {value}
         </button>
     );
