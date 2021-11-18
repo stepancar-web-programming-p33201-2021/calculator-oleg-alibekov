8e53bfe9a8ed2cf25f4df5290ed02918618572c4
olegalibekov <alioleg8@gmail.com>
2021-11-18 10:38:57 +0300

    Refactor percentClickHandler function on App.js

--- src/App.js
+++ src/App.js
@@ -96,12 +96,12 @@ const App = () => {
     };
 
     const percentClickHandler = () => {
-        let num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0;
-        let res = calc.res ? parseFloat(removeSpaces(calc.res)) : 0;
+        const num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0;
+        const res = calc.res ? parseFloat(removeSpaces(calc.res)) : 0;
         setCalc({
             ...calc,
-            num: (num /= Math.pow(100, 1)),
-            res: (res /= Math.pow(100, 1)),
+            num: (num / Math.pow(100, 1)),
+            res: (res / Math.pow(100, 1)),
             sign: "",
         });
     };
